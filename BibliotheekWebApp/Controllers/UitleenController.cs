a16071d8a466cc88526c5c968ad18f16516aa685
Thijs-Dery <thijs.dery@student.ehb.be>
2024-12-15 23:24:21 +0100

    Uitgeleende oeken pagina

---
+++ BibliotheekWebApp/Controllers/UitleenController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using BibliotheekApp.Models;
+using System.Linq;
+
+namespace BibliotheekApp.Controllers
+{
+    [Route("Uitleen")]
+    public class UitleenController : Controller
+    {
+        private readonly BibliotheekContext _context;
+
+        public UitleenController(BibliotheekContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet("UitgeleendeBoeken")]
+        public IActionResult UitgeleendeBoeken()
+        {
+            var uitgeleendeBoeken = _context.LidBoeken
+                .Include(lb => lb.Lid)
+                .Include(lb => lb.Boek)
+                .Where(lb => !lb.Boek.IsDeleted)
+                .ToList();
+
+            return View("~/Views/Uitleen/Index.cshtml", uitgeleendeBoeken);
+        }
+    }
+}
